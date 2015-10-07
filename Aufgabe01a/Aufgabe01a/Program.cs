7097169418909cb9ccbaffe6f0d86581272aca2e
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-10-07 20:14:27 +0200

    add Aufgabe 1+2+3

---
+++ Aufgabe01a/Aufgabe01a/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Aufgabe01a {
+	class MainClass {
+		
+		public static void Main (string[] args) {
+			Console.Out.Write ("Please enter your value: ");
+			string input = Console.In.ReadLine ();
+			Console.Out.Write ("Please enter the value's base: ");
+			int inputBase = readInt ();
+			Console.Out.Write ("Please enter the target base: ");
+			int outputBase = readInt ();
+			Console.Out.WriteLine ("\"{0}\" base:{1} = \"{2}\" base:{3}", input, inputBase, convertFromToBase (input, inputBase, outputBase), outputBase);
+		}
+
+		public static string convertFromToBase(string input, int inputBase, int outputBase){
+			string result = "";
+			int sum = int.Parse(input);
+			if(inputBase != 10)
+				sum = toDecimal(input, inputBase);
+
+			while (sum != 0) {
+				result = getChar(sum, outputBase) + result;
+				sum /= outputBase;
+			}
+
+			return result;				
+		}
+
+		public static char getChar(int sum, int outputBase){
+			if (sum % outputBase > 9) {
+				return Convert.ToChar (sum % outputBase + 55);
+			}
+			return (sum % outputBase).ToString ()[0];
+		}
+
+		public static int toDecimal(string input, int inputBase){
+			int sum = 0;
+
+			Console.Out.WriteLine ("not base 10");
+			for(int i = 0; i < input.Length; i++){
+				int num;
+
+				if ((int)Char.GetNumericValue (input [input.Length - 1 - i]) == -1) {
+					num = (int)input.ToUpper () [input.Length - 1 - i] - 55;
+				} else {
+					num = (int)Char.GetNumericValue (input [input.Length - 1 - i]);
+				}
+
+				sum += num * (int)(Math.Pow (inputBase, i));
+			}
+			return sum;
+		}
+
+		public static int readInt(){
+			int num;
+			bool success = int.TryParse (Console.In.ReadLine (), out num);
+			while (!success || num > 16 || num < 2) {
+				Console.Out.WriteLine ("ERROR: you have to enter a number between 2 - 16");
+				success = int.TryParse (Console.In.ReadLine (), out num);
+			}
+			return num;
+		}
+	}
+}
---
+++ Aufgabe01a/Aufgabe01b/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aufgabe01b {
+	class MainClass {
+		public static void Main () {
+			string input = "010000101101101100111010010001000110101";
+			try{
+				Console.Out.WriteLine ("{0} = {1}", input, decode(input));
+			} catch(Exception e) {
+				Console.Out.WriteLine (e.Message);
+			}
+		}
+
+		public static string decode(string input){
+			checkInput (input);
+			string output = "";
+			int length = 2;
+			Dictionary<string, char> cheatSheet = new Dictionary<string, char> (){
+				{"01001",'A'}, {"10",'F'}, {"01000",'I'}, {"0111",'M'}, {"0101",'N'}, {"11",'O'}, {"0110",'R'}, {"00",'T'}
+			};
+
+			do {
+				foreach (var letter in cheatSheet) {
+					if (input.Substring (0, length) == letter.Key) {
+						input = input.Substring (length, input.Length-length);
+						output += letter.Value;
+						length = 1;
+						break;
+					}
+				}
+				length++;
+				if(length>5)
+					throw new Exception ("input contains not known lettercodes");
+			} while(input.Length > 0);
+
+			return output;
+		}
+
+		public static void checkInput(string input){
+			foreach (char letter in input) {
+				if(letter!='0' && letter!='1')
+					throw new Exception ("input contains forbidden characters");
+			}
+		}
+	}
+}
